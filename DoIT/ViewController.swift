b0e2fcea6eb68b5c0c2f86563cab2c27b4d7c919
Jorge <jlvelasco1994@gmail.com>
2016-11-23 11:22:15 -0600

    Implemented a Table View into our App.

--- DoIT/ViewController.swift
+++ DoIT/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tasksViewController: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasksViewController.dataSource = self
+        tasksViewController.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Place Holder"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
