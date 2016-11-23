5aee8f8c2f1a4fa7177eefd01430f470849cb347
Jorge <jlvelasco1994@gmail.com>
2016-11-23 12:09:58 -0600

    implemented a second view controller with navigation bar that segues the main view controller to the add task view controller. App STILL NEEDS TO PASS INFO.

---
+++ DoIT/Tasks File.swift
@@ -0,0 +1,15 @@
+//
+//  Tasks File.swift
+//  DoIT
+//
+//  Created by Jorge on 11/23/16.
+//  Copyright © 2016 Jorge Velasco Sanvodal. All rights reserved.
+//
+
+import Foundation
+
+class TaskObject {
+    var taskName = ""
+    var important: Bool = false
+}
+
--- DoIT/ViewController.swift
+++ DoIT/ViewController.swift
@@ -12,23 +12,54 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tasksViewController: UITableView!
     
+    var tasks: [TaskObject] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTask()
         
         tasksViewController.dataSource = self
         tasksViewController.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Place Holder"
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "‼️\(task.taskName)"
+        } else {
+            cell.textLabel?.text = task.taskName
+        }
         return cell
     }
+    
+    func makeTask() ->  [TaskObject] {
+        let task1 = TaskObject()
+        task1.taskName = "Finish Learning Swift"
+        task1.important = false
+        
+        let task2 = TaskObject()
+        task2.taskName = "Walk the dog"
+        task2.important = true
+        
+        let task3 = TaskObject()
+        task3.taskName = "Get Xmas Presents"
+        task3.important = false
+        
+        return [task1, task2, task3]
+    }
+    
+    @IBAction func addTaskButton(_ sender: Any) {
+        performSegue(withIdentifier: "addTaskSegue", sender: nil)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
